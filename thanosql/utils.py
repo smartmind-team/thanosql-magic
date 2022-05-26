e846214b297c4a35dc9e49aa37125ec007f1b4ad
Junyoung Yoon <jyoon@connect.ust.hk>
2022-05-26 22:38:27 +0900

    print image title, video update

--- thanosql/utils.py
+++ thanosql/utils.py
@@ -7,22 +7,25 @@ def print_image(df, print_option):
     w = 40
     h = 40
     fig = plt.figure(figsize=(w, h))
-    columns = 10
 
-    column = df.columns[0]
-    rows = int(len(df[column]) / columns) + 1
-    for i, img_path in enumerate(df[column]):
+    column_name = df.columns[0]
+
+    num_cols = 5
+    num_rows = 5
+    df = df[: num_cols * num_rows]
+    for idx, img_path in enumerate(df[column_name]):
         image = imread(img_path)
-        fig.add_subplot(rows, columns, i + 1)
+        fig.add_subplot(num_rows, num_cols, idx + 1)
         plt.axis("off")
+        plt.title(img_path, fontdict={"fontsize": 50 / num_cols})
         plt.imshow(image)
     plt.show()
     return
 
 
 def print_audio(df, print_option):
-    column = df.columns[0]
-    audio_file_list = list(df[column])
+    column_name = df.columns[0]
+    audio_file_list = list(df[column_name])
 
     ### display 5 videos max
     limit = 5
@@ -33,12 +36,12 @@ def print_audio(df, print_option):
 
 
 def print_video(df, print_option):
-    column = df.columns[0]
-    video_file_list = list(df[column])
+    column_name = df.columns[0]
+    video_file_list = list(df[column_name])
 
     ### display 5 videos max
     limit = 5
     for video in video_file_list[:limit]:
         print(video)
-        display(Video(video))
+        display(Video(video, embed=True))
     return
