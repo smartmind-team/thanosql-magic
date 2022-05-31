268b81a93a7cd9fe6893c7821d15429f1ae0e38a
Junyoung Yoon <jyoon@connect.ust.hk>
2022-05-31 18:02:12 +0900

    base dir update

--- thanosql/utils.py
+++ thanosql/utils.py
@@ -3,7 +3,7 @@ from IPython.display import Audio, Video, display
 from matplotlib.pyplot import imread
 
 
-def print_image(df, print_option):
+def print_image(df, print_option, print_base_dir):
     w = 40
     h = 40
     fig = plt.figure(figsize=(w, h))
@@ -13,9 +13,8 @@ def print_image(df, print_option):
     num_cols = 5
     num_rows = 5
     df = df[: num_cols * num_rows]
-    base_dir = print_option.get("base_dir", "")
     for idx, image_path in enumerate(df[column_name]):
-        image_full_path = f"{base_dir}/{image_path}"
+        image_full_path = f"{print_base_dir}/{image_path}"
         image = imread(image_full_path)
         fig.add_subplot(num_rows, num_cols, idx + 1)
         plt.axis("off")
@@ -25,29 +24,27 @@ def print_image(df, print_option):
     return
 
 
-def print_audio(df, print_option):
+def print_audio(df, print_option, print_base_dir):
     column_name = df.columns[0]
     audio_file_list = list(df[column_name])
 
     ### display 5 videos max
     limit = 5
-    base_dir = print_option.get("base_dir", "")
     for audio_path in audio_file_list[:limit]:
-        audio_full_path = f"{base_dir}/{audio_path}"
+        audio_full_path = f"{print_base_dir}/{audio_path}"
         print(audio_full_path)
         display(Audio(audio_full_path))
     return
 
 
-def print_video(df, print_option):
+def print_video(df, print_option, print_base_dir):
     column_name = df.columns[0]
     video_file_list = list(df[column_name])
 
     ### display 5 videos max
     limit = 5
-    base_dir = print_option.get("base_dir", "")
     for video_path in video_file_list[:limit]:
-        video_full_path = f"{base_dir}/{video_path}"
+        video_full_path = f"{print_base_dir}/{video_path}"
         print(video_full_path)
         display(Video(video_full_path, embed=True))
     return
