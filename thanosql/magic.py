83592d0fb88ca1d24a236dc4b22902d24dea6183
Junyoung Yoon <55232334+jyoon0808@users.noreply.github.com>
2022-08-01 19:03:04 +0900

    psql code development  (#20)
    * psql code development + websocket code refactoring
    
    * add request_thanosql_engine for refactoring, and update version
    
    Co-authored-by: Yoon Junyoung <jyoon@sconneect.ust.hk>
    Co-authored-by: Dada Ahn <dadanuna@gmail.com>
    Co-authored-by: Dada Ahn <71164591+lazyduo@users.noreply.github.com>

--- thanosql/magic.py
+++ thanosql/magic.py
@@ -18,8 +18,35 @@ from thanosql.utils import format_result
 DEFAULT_API_URL = "wss://engine.thanosql.ai/ws/v1/query"
 
 
+def request_thanosql_engine(ws, api_url, api_token, query_context):
+    try:
+        ws.connect(f"{api_url}?api_token={api_token}")
+    except:
+        raise ThanoSQLConnectionError("Could not connect to the Websocket")
+
+    ws.send(json.dumps(query_context))
+
+    try:
+        while True:
+            output = ws.recv()
+            output_dict = json.loads(output)
+
+            if output_dict["output_type"] == "MESSAGE":
+                print(output_dict["output_message"])
+            elif output_dict["output_type"] == "RESULT":
+                return format_result(output_dict)
+            elif output_dict["output_type"] == "ERROR":
+                raise ThanoSQLInternalError(output_dict["output_message"])
+
+    except KeyboardInterrupt:
+        ws.close()
+        raise ThanoSQLConnectionError("KeyboardInterrupt")
+
+
 @magics_class
 class ThanosMagic(Magics):
+    ws = websocket.WebSocket()
+
     @needs_local_scope
     @line_cell_magic
     def thanosql(self, line: str = None, cell: str = None, local_ns={}):
@@ -54,43 +81,33 @@ class ThanosMagic(Magics):
             )
 
         query_string = cell
+
         if is_multiple_queries(query_string):
             raise ThanoSQLSyntaxError("Multiple Queries are not supported.")
 
-        if query_string:
-            query_string = convert_local_ns(query_string, local_ns)
+        query_string = convert_local_ns(query_string, local_ns)
+        query_context = {"query_string": query_string, "query_type": "thanosql"}
 
-            data = {"query_string": query_string}
+        return request_thanosql_engine(self.ws, api_url, api_token, query_context)
 
-            try:
-                ws = websocket.WebSocket()
-                ws.connect(f"{api_url}?api_token={api_token}")
+    @needs_local_scope
+    @line_cell_magic
+    def psql(self, line: str = None, cell: str = None, local_ns={}):
+        if line:
+            cell = line
 
-                ws.send(query_string)
-            except:
-                raise ThanoSQLConnectionError("Could not connect to the Websocket")
-                ws.close()
+        if not cell:
+            return
 
-            connection_open = True
-            try:
-                while connection_open:
-                    output = ws.recv()
-                    output_dict = json.loads(output)
-                    if output_dict["output_type"] == "ERROR":
-                        raise ThanoSQLInternalError(output_dict["output_message"])
-                    elif output_dict["output_type"] == "CONNECTION_CLOSE":
-                        break
-                    elif output_dict["output_type"] == "PING":
-                        continue
-                    elif output_dict["output_type"] == "RESULT":
-                        return format_result(output_dict)
-                    else:
-                        print(output_dict["output_message"])
-            except KeyboardInterrupt:
-                ws.close()
-                raise Exception("KEYBOARD INTERRUPTION, TASK KILLED")
+        api_url = os.getenv("API_URL", DEFAULT_API_URL)
+        api_token = os.getenv("API_TOKEN", None)
 
-        return
+        query_string = cell
+
+        query_string = convert_local_ns(query_string, local_ns)
+        query_context = {"query_string": query_string, "query_type": "psql"}
+
+        return request_thanosql_engine(self.ws, api_url, api_token, query_context)
 
 
 # In order to actually use these magics, you must register them with a
